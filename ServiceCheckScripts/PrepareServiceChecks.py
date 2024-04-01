946c8b7a4045e5fe236fc6ab3af300efa5547e46
Jeremiah Webb <illusion173@hotmail.com>
2024-04-01 12:51:35 -0400

    Finished DB Connector, cleanup

--- ServiceCheckScripts/PrepareServiceChecks.py
+++ ServiceCheckScripts/PrepareServiceChecks.py
@@ -10,25 +10,23 @@ def prepare_service_check(loaded_env_dict: dict) -> list:
         for target in target_list:
             # We need to grab the actions list
             actions_list = target_list["ACTIONS"]
-            # We need to create a new Service Check
-
             for action in actions_list:
                 new_ssh_info = None
 
-                if target["SERVICE_NAME"] == "SSH":
+                if action["SERVICE_NAME"] == "SSH":
                     new_ssh_info = Results.SSHInfo()
-                    new_ssh_info.ssh_username = target["SSH_USERNAME"]
-                    new_ssh_info.ssh_priv_key = target["SSH_PRIV_KEY"]
-                    new_ssh_info.md5sum = target["MD5_SUM"]
-                    new_ssh_info.ssh_script = target["SSH_SCRIPT"]
+                    new_ssh_info.ssh_username = action["SSH_USERNAME"]
+                    new_ssh_info.ssh_priv_key = action["SSH_PRIV_KEY"]
+                    new_ssh_info.md5sum = action["MD5_SUM"]
+                    new_ssh_info.ssh_script = action["SSH_SCRIPT"]
 
                 new_service_health_check = Results.ServiceHealthCheck(
                     target_host=str(target["IP"]),
-                    target_port=str(target.get("PORT", None)),
+                    target_port=str(action.get("PORT", None)),
                     team_id=str(team["TEAM_ID"]),
                     team_name=str(team["TEAM_NAME"]),
                     ssh_info=new_ssh_info,
-                    service_name=target["SERVICE_NAME"],
+                    service_name=action["SERVICE_NAME"],
                 )
                 prepared_service_checks.append(new_service_health_check)
 
